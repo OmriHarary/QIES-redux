83b28c3bb3d02a7ecbb5a60ea7d87e130a6cd1b5
OmriHarary <omriharary@hotmail.com>
2020-06-26 18:56:54 -0400

    Created project files for frontend

---
+++ QIES.Frontend/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QIES.Frontend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

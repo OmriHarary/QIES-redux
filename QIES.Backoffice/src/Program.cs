21b21b265f2abf8e328a14008f687b3c310199c0
OmriHarary <omriharary@hotmail.com>
2020-07-01 21:22:05 -0400

    Created project files for backoffice component

---
+++ QIES.Backoffice/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QIES.Backoffice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

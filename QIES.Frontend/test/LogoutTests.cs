4b05793bf610e0f44283dc2e6d21ea1de936fd86
OmriHarary <omriharary@hotmail.com>
2020-07-14 21:05:08 -0400

    Implemented tests for logout

---
+++ QIES.Frontend/test/LogoutTests.cs
@@ -0,0 +1,52 @@
+using System;
+using QIES.Frontend.Session;
+using QIES.Frontend.Transaction;
+using Xunit;
+
+namespace QIES.Frontend.Tests
+{
+    public class LogoutTests
+    {
+        private SessionController controller;
+
+        public LogoutTests()
+        {
+            controller = new SessionController();
+        }
+
+        [Fact]
+        public void Logout_AsAgent_LogoutSuccess()
+        {
+            controller.ActiveLogin = LoginType.AGENT;
+            var request = new LogoutRequest(); 
+
+            var (success, _, login) = controller.ProcessLogout(request);
+
+            Assert.True(success);
+            Assert.Equal(LoginType.NONE, login);
+        }
+
+        [Fact]
+        public void Logout_AsPlanner_LogoutSuccess()
+        {
+            controller.ActiveLogin = LoginType.PLANNER;
+            var request = new LogoutRequest(); 
+
+            var (success, _, login) = controller.ProcessLogout(request);
+
+            Assert.True(success);
+            Assert.Equal(LoginType.NONE, login);
+        }
+
+        [Fact]
+        public void Logout_NotLoggedIn_LogoutSuccess()
+        {
+            controller.ActiveLogin = LoginType.NONE;
+            var request = new LogoutRequest(); 
+
+            var (success, _, _) = controller.ProcessLogout(request);
+
+            Assert.False(success);
+        }
+    }
+}
